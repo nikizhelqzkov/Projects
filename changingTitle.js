2ea5f016d744faf94ee612cb8cb586d81ab6bf4a
nikizhelqzkov <nikizhelqzkov@gmail.com>
2021-05-13 18:47:23 +0300

    adding index.html and images

---
+++ changingTitle.js
@@ -0,0 +1,12 @@
+function getWidth() {
+    return Math.max(
+        document.body.scrollWidth,
+        document.documentElement.scrollWidth,
+        document.body.offsetWidth,
+        document.documentElement.offsetWidth,
+        document.documentElement.clientWidth
+    )
+}
+if(getWidth()<500){
+    document.title = `Mobile edition`;
+}
