7969745cc7019d1d8ffdf6bc39b75756f597bbad
nikizhelqzkov <nikizhelqzkov@gmail.com>
2021-05-14 17:01:41 +0300

    adding social logos and effects on footer

---
+++ assets/js/hoverChangeLogos.js
@@ -0,0 +1,22 @@
+let specialNumber = 0;
+
+const onHover = (element) => {
+  if (specialNumber === 0) {
+    ++specialNumber;
+    let src = element.src;
+    src = src.slice(0, src.length - 4);
+    src += "-hover.svg";
+    element.src = src;
+  }
+};
+
+const offHover = (element) => {
+  if (specialNumber === 1) {
+    --specialNumber;
+    let src = element.src;
+    src = src.slice(0, src.length - 10);
+    src+=".svg";
+    element.src = src;
+
+  }
+};
